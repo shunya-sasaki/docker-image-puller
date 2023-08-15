c9107d217a7f7132a8445cdbe1e15f2f1ce1ab45
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2023-08-15 17:25:45 +0900

    Add proxy and encoding options

--- src/imagepuller/run.py
+++ src/imagepuller/run.py
@@ -4,12 +4,18 @@ from imagepuller.puller import DockerImagePuller
 parser = argparse.ArgumentParser(
     description="Pull docker image with out Docker"
 )
-parser.add_argument("--image", "-i", help="Image name to pull")
-parser.add_argument("--tag", "-t", help="Image tag to pull")
+parser.add_argument("--image", "-i", help="Image name to pull", required=True)
+parser.add_argument("--tag", "-t", help="Image tag to pull", required=True)
 parser.add_argument(
     "--architecture", "-a", help="Architecture to pull", default="amd64"
 )
 parser.add_argument("--variant", "-v", help="Variant to pull", default="")
+parser.add_argument(
+    "--proxy", "-p", help="Proxy to use", default=None, type=str
+)
+parser.add_argument(
+    "--encoding", "-e", help="Encoding to use", default="utf-8", type=str
+)
 
 
 def run(
@@ -17,6 +23,8 @@ def run(
     tag: str = None,
     architecture: str = None,
     variant: str = None,
+    proxy: str | None = None,
+    encoding: str = "utf-8",
 ):
     args = parser.parse_args()
     if name is None:
@@ -27,6 +35,10 @@ def run(
         architecture = args.architecture
     if variant is None:
         variant = args.variant
+    if proxy is None:
+        proxy = args.proxy
+    if encoding == "utf-8":
+        encoding = args.encoding
     puller = DockerImagePuller(
         name=name,
         tag=tag,
